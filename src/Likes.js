c03b16f1022ddc661d8db1e7470b9d8815f80f28
pamphilemkp <pamphilemkp@gmail.com>
2022-08-03 00:02:17 +0300

    add getting likes from the api

---
+++ src/Likes.js
@@ -0,0 +1,8 @@
+
+//getting the likes from the API and displaying the clicked Element
+export const GetLikes = async (id , nextEL) => {
+    const value = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/xxyYZnyEypPJCG46fkIR/likes');
+    const ArrValue = await value.json();
+    nextEL.innerHTML = ArrValue[id].likes;
+};
+
--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 import './style.css';
+import {GetLikes} from './Likes.js';
 
 
 
 
 const body = document.querySelector(".contents");
-  const getInfos = async() => {
+let  LikeID = 0;
+
+// display the cards
+ export const getInfos = async() => {
    const result = await fetch('https://zoo-animal-api.herokuapp.com/animals/rand/10');
     const response = await result.json();
     console.log(response)
     response.forEach(element => {
         let div= document.createElement('div');
+        div.classList.add('card');
     div.innerHTML = `<img src=${element.image_link} width="300px" height="300px"> <br/>
+                    <p class="likes" id=${LikeID}>💗</p>
+                    <p> 0 likes <p>
                     <h2>${element.name}</h2>
-                    <p>${element.diet}</p>`;
-
+                    <p>${element.diet}</p>
+                    <button class="comments">Comments</button>`;
+                    
+                    LikeID ++;
         body.appendChild(div);
     });
   }
 
+  // send the information to get element from  API
+
+  body.addEventListener('click',(e) =>{
+    
+    const id = JSON.parse(e.target.id);
+    const NextEl = e.target.nextElementSibling;
+
+    GetLikes(id , NextEl);
+  })
+
   getInfos();
+
   
