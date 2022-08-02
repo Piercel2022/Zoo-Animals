cc21cacc949d56618d12619635ec02d5f1a038ef
pamphilemkp <pamphilemkp@gmail.com>
2022-08-03 00:05:37 +0300

    add likes to the website

--- src/Likes.js
+++ src/Likes.js
@@ -3,6 +3,6 @@
 export const GetLikes = async (id , nextEL) => {
     const value = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/xxyYZnyEypPJCG46fkIR/likes');
     const ArrValue = await value.json();
-    nextEL.innerHTML = ArrValue[id].likes;
+    nextEL.innerHTML = `${ArrValue[id].likes } likes`;
 };
 
