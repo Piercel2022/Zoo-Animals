986481af97494568904a91b4d8a90b86e414ac49
pamphilemkp <pamphilemkp@gmail.com>
2022-08-03 15:28:44 +0300

    add addlikes and display them

--- src/Likes.js
+++ src/Likes.js
@@ -1,8 +1,9 @@
+// getting the likes from the API and displaying the clicked Element
+// eslint-disable-next-line import/prefer-default-export
 
-//getting the likes from the API and displaying the clicked Element
-export const GetLikes = async (id , nextEL) => {
-    const value = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/xxyYZnyEypPJCG46fkIR/likes');
-    const ArrValue = await value.json();
-    nextEL.innerHTML = `${ArrValue[id].likes } likes`;
+export const GetLikes = async (id, nextEL) => {
+  const value = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/xxyYZnyEypPJCG46fkIR/likes');
+  const ArrValue = await value.json();
+  nextEL.innerHTML = `${ArrValue[id].likes} likes`;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,42 +1,44 @@
 import './style.css';
-import {GetLikes} from './Likes.js';
+import { GetLikes} from './Likes.js';
+import { addLikes } from './AddLikes.js';
 
 
-
-
-const body = document.querySelector(".contents");
-let  LikeID = 0;
+const body = document.querySelector('.contents');
+let LikeID = 0;
 
 // display the cards
- export const getInfos = async() => {
-   const result = await fetch('https://zoo-animal-api.herokuapp.com/animals/rand/10');
-    const response = await result.json();
-    console.log(response)
-    response.forEach(element => {
-        let div= document.createElement('div');
-        div.classList.add('card');
+export const getInfos = async () => {
+  const result = await fetch('https://zoo-animal-api.herokuapp.com/animals/rand/10');
+  const response = await result.json();
+  // console.log(response)
+  response.forEach((element) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
     div.innerHTML = `<img src=${element.image_link} width="300px" height="300px"> <br/>
                     <p class="likes" id=${LikeID}>💗</p>
                     <p> 0 likes <p>
                     <h2>${element.name}</h2>
                     <p>${element.diet}</p>
                     <button class="comments">Comments</button>`;
-                    
-                    LikeID ++;
-        body.appendChild(div);
-    });
+
+    LikeID++
+    body.appendChild(div);
+
+    // popup
+  });
+};
+
+// send the information to get element from  API
+
+body.addEventListener('click', (e) => {
+  const id = JSON.parse(e.target.id);
+  const NextEl = e.target.nextElementSibling;
+  if (e.target.id) {
+    GetLikes(id, NextEl);
+    addLikes(e.target.id);
   }
+});
 
-  // send the information to get element from  API
+// document.addEventListener('DOMContentLoaded', GetLikes());
 
-  body.addEventListener('click',(e) =>{
-    
-    const id = JSON.parse(e.target.id);
-    const NextEl = e.target.nextElementSibling;
-
-    GetLikes(id , NextEl);
-  })
-
-  getInfos();
-
-  
+getInfos();
